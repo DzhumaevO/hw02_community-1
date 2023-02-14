4a131aa6671609e1c9e778cc42ca379bff99297f
Бутенко Ниолай <e2e4e2e4@mail.ru>
2023-02-14 15:22:06 +0200

    yatube_project

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -11,14 +11,12 @@ class Post(models.Model):
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        related_name='posts'
-        )
+        related_name='posts')
     group = models.ForeignKey(
         'Group',
         blank=True,
         null=True,
-        on_delete=models.CASCADE,
-        )
+        on_delete=models.CASCADE)
 
 
 class Group(models.Model):
