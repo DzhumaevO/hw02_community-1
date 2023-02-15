f7588b262ceced302bd7bcba20e3032e517d3724
Бутенко Николай <e2e4e2e4@mail.ru>
2023-02-15 22:15:22 +0200

    final1

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Post, Group
+from .models import Group, Post
 
 
+@admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ('pk', 'text', 'pub_date', 'author', 'group')
     list_editable = ('group',)
@@ -10,11 +11,6 @@ class PostAdmin(admin.ModelAdmin):
     empty_value_display = '-пусто-'
 
 
-admin.site.register(Post, PostAdmin)
-
-
+@admin.register(Group)
 class GroupAdmin(admin.ModelAdmin):
     list_display = ('title', 'slug', 'description')
-
-
-admin.site.register(Group)
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,20 +1,20 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post, Group
 
+number_last_posts = 10
+
 
 def index(request):
-    posts = Post.objects.order_by('-pub_date')[:10]
-    title = 'Последние обновления на сайте'
+    posts = Post.objects.order_by('-pub_date')[:number_last_posts]
     context = {
-        'posts': posts,
-        'title': title
+        'posts': posts
     }
     return render(request, 'posts/index.html', context)
 
 
-def group_posts(request, slug):
-    group = get_object_or_404(Group, slug=slug)
-    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+def group_posts(request, slug_name):
+    group = get_object_or_404(Group, slug=slug_name)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:number_last_posts]
     title = "Здесь будет информация о группах проекта Yatube"
     template = 'posts/group_list.html'
     context = {
