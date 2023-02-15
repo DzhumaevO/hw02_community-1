d86ed1dc3e83ec5addd74eaddfe4c318bbd88678
Бутенко Николай <e2e4e2e4@mail.ru>
2023-02-15 22:21:00 +0200

    final1

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post, Group
 
-number_last_posts = 10
+last_posts = 10
 
 
 def index(request):
-    posts = Post.objects.order_by('-pub_date')[:number_last_posts]
+    posts = Post.objects.order_by('-pub_date')[:last_posts]
     context = {
         'posts': posts
     }
@@ -14,7 +14,7 @@ def index(request):
 
 def group_posts(request, slug_name):
     group = get_object_or_404(Group, slug=slug_name)
-    posts = Post.objects.filter(group=group).order_by('-pub_date')[:number_last_posts]
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:last_posts]
     title = "Здесь будет информация о группах проекта Yatube"
     template = 'posts/group_list.html'
     context = {
