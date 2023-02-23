1e96ecf7cdb89b7d2ec1a37ab69c6e8a49973509
Бутенко Николай <e2e4e2e4@mail.ru>
2023-02-23 17:31:37 +0200

    fin

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -33,6 +33,8 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
+    'core.apps.CoreConfig',
+    'users.apps.UsersConfig',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
